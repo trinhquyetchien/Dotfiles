313ddc5b1b812429158d9f122686b2a77a6330a7
trinhquyetchien <trinhquyetchien2005@gmail.com>
2026-05-24 22:00:48 +0700

    feat(alias): add google chrome alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -4,9 +4,9 @@ alias lzd="lazydocker"
 alias lzs="lazysql"
 alias ..="cd .."
 alias note=" cd ~/Note; v"
-alias gg='google-chrome https://calendar.google.com'
 alias cl="clear"
 alias .="cd"
+alias sz='source ~/.zshrc'
 
 # Android/Emulator
 alias emu="emulator -list-avds"
@@ -17,3 +17,11 @@ alias sleep='systemctl suspend'              # Tạm dừng (ngủ nhẹ)
 alias shutdown='sudo poweroff'                 # Tắt máy hoàn toàn
 alias out='gnome-session-quit --logout' # Đăng xuất nhanh
 alias lock='loginctl lock-session'  # Khóa màn hình
+
+alias email='google-chrome https://mail.google.com/mail'
+alias zalo='google-chrome https://chat.zalo.me'
+alias mess='google-chrome https://www.messenger.com'
+alias driver='google-chrome https://drive.google.com'
+alias calendar='google-chrome https://calendar.google.com'
+alias youtube='google-chrome https://youtube.com'
+alias github='google-chrome https://github.com'
