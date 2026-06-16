214926a57e4567bc9ed88ce03d69d04a3332a647
trinhquyetchien <trinhquyetchien2005@gmail.com>
2026-06-16 23:39:51 +0700

    update(wezterm): attach wezterm tab and custom tab line

--- wezterm/.config/wezterm/keybindings.lua
+++ wezterm/.config/wezterm/keybindings.lua
@@ -1,20 +1,83 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local M = {}
 
 function M.apply_to_config(config)
-    config.keys = {
-        -- Opacity controls
-        { key = "UpArrow",    mods = "CTRL|SHIFT", action = wezterm.action.EmitEvent("opacity-up") },
-        { key = "DownArrow",  mods = "CTRL|SHIFT", action = wezterm.action.EmitEvent("opacity-down") },
-        { key = "Space",      mods = "CTRL|SHIFT", action = wezterm.action.EmitEvent("opacity-reset") },
-        
-        -- Window controls
-        { key = "Enter",      mods = "CTRL|SHIFT", action = "ToggleFullScreen" },
-        
-        -- Clipboard
-        { key = "C",          mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
-        { key = "V",          mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") }
-    }
+	config.keys = {
+		-- Opacity controls
+		{
+			key = "UpArrow",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.EmitEvent("opacity-up"),
+		},
+		{
+			key = "DownArrow",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.EmitEvent("opacity-down"),
+		},
+		{
+			key = "Space",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.EmitEvent("opacity-reset"),
+		},
+
+		-- Fullscreen
+		{
+			key = "Enter",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ToggleFullScreen,
+		},
+
+		-- Clipboard
+		{
+			key = "C",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.CopyTo("Clipboard"),
+		},
+		{
+			key = "V",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.PasteFrom("Clipboard"),
+		},
+
+		-- Tạo tab mới
+		{
+			key = "T",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+		},
+
+		-- Đóng tab hiện tại
+		{
+			key = "W",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.CloseCurrentTab({
+				confirm = false,
+			}),
+		},
+		{
+			key = "E",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.PromptInputLine({
+				description = "Rename Tab",
+				action = wezterm.action_callback(function(window, pane, line)
+					if line then
+						window:active_tab():set_title(line)
+					end
+				end),
+			}),
+		},
+		{
+			key = "h",
+			mods = "ALT",
+			action = wezterm.action.ActivateTabRelative(-1),
+		},
+
+		{
+			key = "l",
+			mods = "ALT",
+			action = wezterm.action.ActivateTabRelative(1),
+		},
+	}
 end
 
 return M
--- wezterm/.config/wezterm/options.lua
+++ wezterm/.config/wezterm/options.lua
@@ -1,30 +1,52 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local M = {}
 
 function M.apply_to_config(config)
-    -- Required for IME preedit/compose in terminal apps (fcitx5/ibus).
-    config.use_ime = true
+	config.use_ime = true
 
-    -- Font & Cỡ chữ
-    config.font = wezterm.font_with_fallback({
-        { family = "JetBrains Mono", weight = "Medium" },
-        { family = "Fira Code" },
-    })
-    config.font_size = 12.0
-    config.line_height = 1.2
+	config.window_padding = {
+		left = 3,
+		right = 3,
+		top = 0,
+		bottom = 0,
+	}
 
-    -- Giao diện & Màu sắc
-    config.color_scheme = "Kanagawa Dragon"
-    config.window_padding = { left = 2, right = 2, top = 0, bottom = 0 }
-    config.enable_tab_bar = false
-    config.warn_about_missing_glyphs = false
-    config.window_decorations = "RESIZE"
+	config.font = wezterm.font_with_fallback({
+		{ family = "JetBrains Mono", weight = "Medium" },
+		{ family = "Fira Code" },
+	})
 
-    -- Opacity mặc định
-    config.window_background_opacity = 0.8
+	config.colors = {
+		tab_bar = {
+			background = "#181616",
 
-    -- Shell mặc định
-    config.default_prog = { "/bin/zsh", "-l" }
+			active_tab = {
+				bg_color = "#181616",
+				fg_color = "#DCD7BA",
+			},
+
+			inactive_tab = {
+				bg_color = "#181616",
+				fg_color = "#727169",
+			},
+		},
+	}
+
+	config.font_size = 12.0
+	config.line_height = 1.2
+
+	config.color_scheme = "Kanagawa Dragon"
+	config.window_padding = { left = 2, right = 2, top = 0, bottom = 0 }
+	config.warn_about_missing_glyphs = false
+	config.enable_tab_bar = true
+	config.hide_tab_bar_if_only_one_tab = false
+	config.use_fancy_tab_bar = false
+	config.window_decorations = "RESIZE"
+	config.default_cursor_style = "BlinkingBlock"
+
+	config.window_background_opacity = 0.8
+
+	config.default_prog = { "/bin/zsh", "-l" }
 end
 
 return M
